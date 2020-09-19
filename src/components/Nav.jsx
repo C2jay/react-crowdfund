40c22c5a5b32bef4343f98597568fcf137ca58f8
C2jay <cat.crane15@gmail.com>
2020-09-19 12:53:45 +0800

    css futzing

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import "./Nav.css";
+
 
 
 function Nav() {
     return (
-        <nav>
-            <Link to="/">Home</Link>
-            <Link to="/project">Project</Link>
+        <nav className='nav-container'>
+            <nav id='nav-text'>
+                <Link to="/">Home</Link>
+                <Link to="/project">Project</Link>
+            </nav>
+            <nav className="image-bar"></nav>
         </nav>
     );
 }
