5f8cc4ad002a46db972ca64a7512130ea00e22be
C2jay <cat.crane15@gmail.com>
2020-09-24 16:01:11 +0800

    landingpage css header done

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,7 +8,7 @@ import image2 from '../components/images/image2.jpg';
 import image3 from '../components/images/image3.jpg';
 import image4 from '../components/images/image4.jpg';
 import image5 from '../components/images/image5.jpg';
-import slogan from '../components/images/slogan.png';
+import Logo from '../components/logo/sugarmomma.png';
 
 
 
@@ -29,25 +29,31 @@ function HomePage() {
 
     return (
         <div>
-        <div className='header-bar'>
-            <div className='slogan'>
-                <h3>Life is Sweet with Sugar Momma</h3>
+            <div className='header-bar'>
+               
+                <div className='carousel'>
+                    <AliceCarousel autoPlay autoPlayInterval="3000">
+                        <img src={image1} className="sliderimg"/>
+                        <img src={image2} className="sliderimg"/>
+                        <img src={image3} className="sliderimg"/>
+                        <img src={image4} className="sliderimg"/>
+                        <img src={image5} className="sliderimg"/>
+                    </AliceCarousel>
+                </div>
+
+                <div className='slogan'>
+                    <h3>Life is sweet with Sugar Momma!</h3>
+                    <img src={Logo} />
+                    <p>post your project images to instagram with the tag #thnxsugarmomma for a chance to get your image featured here!</p>
+                </div>
+
             </div>
-            <div className='carousel'>
-                <AliceCarousel autoPlay autoPlayInterval="3000">
-                    <img src={image1} className="sliderimg"/>
-                    <img src={image2} className="sliderimg"/>
-                    <img src={image3} className="sliderimg"/>
-                    <img src={image4} className="sliderimg"/>
-                    <img src={image5} className="sliderimg"/>
-                </AliceCarousel>
-            </div>
-        </div>
-            <div id="project-list">
-                {projectList.map((projectData, key) => {
-                    return <ProjectCard key={key} projectData={projectData} />;
-                })}
-            </div>
+                
+                <div id="project-list">
+                    {projectList.map((projectData, key) => {
+                        return <ProjectCard key={key} projectData={projectData} />;
+                    })}
+                </div>
         </div>
     );
 }
