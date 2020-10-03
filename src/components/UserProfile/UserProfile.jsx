6d67b1cef895b93fbcea3da251fff11e3d86dcc5
C2jay <cat.crane15@gmail.com>
2020-10-03 19:05:20 +0800

    stuff

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams } from "react-router-dom";
 
 
 function UserProfile() {
+    const [UserData, setUserData] = useState({ projects: [] });
+    const { id } = useParams();
+    useEffect(() => {
+        fetch(`${process.env.REACT_APP_API_URL}users/${id}`)
+        .then((results) => {
+            return results.json();
+        })
+        .then((data) => {
+            setUserData(data);
+            });
+    }, []);
+
+
+
     return (
-        <h1>This is the user Page</h1>
+    <h1>{ UserData.username }</h1>
     );
 }
 
