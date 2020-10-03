c39c680e287dcc23df4675cde29e68b7e04ef784
C2jay <cat.crane15@gmail.com>
2020-10-03 16:02:37 +0800

    userprofile

---
+++ src/components/UserProfile/UserProfile.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+function UserProfile() {
+    return (
+        <h1>This is the user Page</h1>
+    );
+}
+
+
+export default UserProfile;
---
+++ src/pages/UserProfilePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import UserProfile from '../components/UserProfile/UserProfile';
+
+
+function UserProfile() {
+    return (
+        <UserProfile />
+    );
+}
+
+
+export default UserProfilePage;
